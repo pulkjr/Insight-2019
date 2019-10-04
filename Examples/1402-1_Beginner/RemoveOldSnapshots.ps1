bef53868a0a0fd8b7f08aa63751c69d3b13f9964
Jason Cole <jason@jasnic.com>
2019-10-04 11:29:04 -0600

    Added 1402-1 content and other minor updates.

---
+++ Examples/1402-1_Beginner/RemoveOldSnapshots.ps1
@@ -0,0 +1,21 @@
+# This assumes the DataONTAP module is already imported and the $global:CurrentNcController variable is populated
+# with at least 1 connection object.
+param (
+    [string[]]$Vserver,
+
+    [string[]]$Volume,
+
+    [int]$DaysToKeep = 30
+)
+
+$params = @{ }
+
+if ($Vserver) {
+    $params.Add('Vserver', $Vserver)
+}
+
+if ($Volume) {
+    $params.Add('Volume', $Volume)
+}
+
+Get-NcSnapshot @params | Where-Object { $_.Created -lt [datetime]::Now.AddDays("-$DaysToKeep") } | Remove-NcSnapshot -WhatIf
